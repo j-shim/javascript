ea1ca9a6381ab13b060e6e70b96e7325d357acef
June Shim <jys2@sfu.ca>
2018-05-17 16:10:16 -0700

    modified git script

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -4,14 +4,16 @@ if [ $# -eq 0 ]; then
   echo "----------------------------------------------------------"
   echo "This script runs git add, commit and push with user input."
   echo "----------------------------------------------------------"
-  echo "Usage: RUN ./git_commit_push.sh \"commit message\" file1 file2 ..."
+  echo "Usage: \$ ./git_commit_push.sh \"commit message\" file1 file2 ..."
   echo "  Current folder will be staged if no file input is given."
   exit 1
 fi
 if [ $# -eq 1 ]; then
-  echo "------> Executing \$ git commit -a -m \"$1\"..."
-  git commit -a -m "$1"
-  echo "------> Executing \$ git push..."
+  echo "------> Executing \$ git add ./ ..."
+  git add .
+  echo "------> Executing \$ git commit -m \"$1\" ..."
+  git commit -m "$1"
+  echo "------> Executing \$ git push ..."
   git push
   echo "------> Successful. Exiting..."
 fi
